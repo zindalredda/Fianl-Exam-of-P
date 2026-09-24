651eee7a31a18122f79537c635a6b2255b7fd3ed
Zindda_ <zindalredda@gmail.com>
2026-09-24 23:14:50 +0900

    카드 데이터 Returner 작성

---
+++ Assets/Scripts/Card/CardDataLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Card.Enums;
+using UnityEngine;
+
+namespace Card
+{
+    public static class CardDataLoader
+    {
+        public static int GetTime(this MainCardType cardType, Enum subCardType)
+        {
+            return cardType switch
+            {
+                MainCardType.Major => 4,
+                MainCardType.Liberal => 2,
+                MainCardType.Play => subCardType switch
+                {
+                    PlayType.Cat => 1,
+                    PlayType.Apple => 2,
+                    PlayType.Crane => 3,
+                    PlayType.Cafe => 3,
+                    PlayType.Karaoke => 4,
+                    PlayType.Games => 5,
+                    _ => throw new Exception("Unknown card type")
+                },
+                MainCardType.Work => 6,
+                _ => throw new Exception("Unknown card type")
+            };
+        }
+
+        public static int GetStress(this MainCardType cardType, Enum subCardType)
+        {
+            return cardType switch
+            {
+                MainCardType.Major => 3,
+                MainCardType.Liberal => 3,
+                MainCardType.Play => -4,
+                MainCardType.Work => 6,
+                _ => throw new Exception("Unknown card type")
+            };
+        }
+    }
+}
